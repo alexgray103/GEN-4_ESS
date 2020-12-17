fdf25c7432661708450267858f9538bb22891557
alexgray103 <61517780+alexgray103@users.noreply.github.com>
2020-12-17 12:16:39 -0500

    Update ESS_main.py

--- GUI/ESS_main.py
+++ GUI/ESS_main.py
@@ -1,5 +1,15 @@
+'''
+main.py is where the actual main program is called. Each module class is called in the first lines. A serial port is 
+
+
+
+
+'''
+
 import serial
 from time import sleep
+
+# import all module scripts 
 from ESS_GUI_module_0 import *
 from ESS_GUI_module_1 import *
 from ESS_GUI_module_2 import *
@@ -10,11 +20,13 @@ from ESS_GUI_module_6 import *
 from ESS_GUI_module_7 import *
 
 import matplotlib.pyplot as plt
+#import tkinter library for running the modules
 from tkinter import *
 from tkinter import messagebox
 import tkinter.font as tkfont
 
 # error popup box
+#if serial port cannot be opened pop up this error box
 def spectrometer_disconnect():
     root = Tk()
     root.geometry('800x480')
@@ -23,9 +35,11 @@ def spectrometer_disconnect():
     message = Label(root, text = 'Spectrometer Not connected, Connect and try again', bg = 'sky blue', anchor = "center").pack()
     quit_button = Button(root, text = 'QUIT', command = root.destroy, fg = 'red').pack()
 
+# after the serial port is opened read in the module number and run the corresponding program 
 def run_program():
     sleep(2.5) # wait for a little to initialize serial connection
 
+    #tell arduino to read in the module number 
     ser.write(b'module\n') 
     module = int(ser.readline().decode()) # read in the module number
     
@@ -59,18 +73,22 @@ def run_program():
     root.mainloop()
     
 # open up a serial to allow for reading in of module attachment
+# two possible serial ports on raspberry pi
 port = "/dev/ttyUSB0"
 port2 = "/dev/ttyUSB1"
 run_it = 0 # handler for starting programs
 
-
+# try to open up port number 1 if it works call the run_program function
 try:
     ser = serial.Serial(port, baudrate = 115200, timeout = 3)
     run_it = 1
     run_program()
 except:
     pass
-#check for spectrometer connection if it doesn work 
+
+
+# if the first serial port doesnt work then try the second port2 to run program
+# if both ports dont work then run the error function to popup error
 if run_it == 1:
     pass
 else:
